a8b55cdded94ddff01a396063af388b5eac378bc
DonnieAndMooie <owenjeaves@gmail.com>
2023-05-03 13:18:13 +0100

    Create controllers

--- models/book.js
+++ models/book.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const { Schema } = mongoose;
 
 const BookSchema = new Schema({
-    title: {type: String, required: true},
-    author: {type: Schema.Types.ObjectId, ref: "Author", required: true},
-    summary: {type: String, required: true},
-    isbn: {type: String, required: true},
-    genre: [{type: Schema.Types.ObjectId, ref: "Genre"}]
-})
+  title: { type: String, required: true },
+  author: { type: Schema.Types.ObjectId, ref: "Author", required: true },
+  summary: { type: String, required: true },
+  isbn: { type: String, required: true },
+  genre: [{ type: Schema.Types.ObjectId, ref: "Genre" }],
+});
 
-BookSchema.virtual("url").get(function(){
-    return `/catalog/book/${this._id}`
-})
+BookSchema.virtual("url").get(function () {
+  return `/catalog/book/${this._id}`;
+});
 
-module.exports = mongoose.model("Book", BookSchema)
+module.exports = mongoose.model("Book", BookSchema);
--- models/genre.js
+++ models/genre.js
@@ -1,13 +1,15 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const { Schema } = mongoose;
 
 const GenreSchema = new Schema({
-    name: {type: String, required: true, minLength: 3, maxLength: 100}
-})
+  name: {
+    type: String, required: true, minLength: 3, maxLength: 100,
+  },
+});
 
-GenreSchema.virtual("url").get(function(){
-    return `/catalog/genre/${this._id}`
-})
+GenreSchema.virtual("url").get(function () {
+  return `/catalog/genre/${this._id}`;
+});
 
-module.exports = mongoose.model("Genre", GenreSchema)
+module.exports = mongoose.model("Genre", GenreSchema);
