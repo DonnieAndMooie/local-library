aa4838f881ccc68be41f5d3d99ee743c0e69215f
DonnieAndMooie <owenjeaves@gmail.com>
2023-05-03 23:07:39 +0100

    Create authors list page

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const { Schema } = mongoose;
 
@@ -22,6 +23,14 @@ AuthorSchema.virtual("name").get(function () {
 
 AuthorSchema.virtual("url").get(function () {
   return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : "";
+});
+
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : "";
 });
 
 module.exports = mongoose.model("Author", AuthorSchema);
