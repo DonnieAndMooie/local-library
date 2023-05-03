0ce95f3fc159dc26a583c8338e5f5cce544c2dc2
DonnieAndMooie <owenjeaves@gmail.com>
2023-05-03 15:06:21 +0100

    Create homepage

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -6,7 +6,27 @@ const Genre = require("../models/genre");
 const BookInstance = require("../models/bookinstance");
 
 exports.index = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Site Home Page");
+  const [numBooks,
+    numBookInstances,
+    numAvailableBookInstances,
+    numAuthors,
+    numGenres,
+  ] = await Promise.all([
+    Book.countDocuments({}).exec(),
+    BookInstance.countDocuments({}).exec(),
+    BookInstance.countDocuments({ status: "Available" }).exec(),
+    Author.countDocuments({}).exec(),
+    Genre.countDocuments({}).exec(),
+  ]);
+
+  res.render("index", {
+    title: "Local Library Home",
+    book_count: numBooks,
+    book_instance_count: numBookInstances,
+    book_instance_available_count: numAvailableBookInstances,
+    author_count: numAuthors,
+    genre_count: numGenres,
+  });
 });
 
 exports.book_list = asyncHandler(async (req, res, next) => {
