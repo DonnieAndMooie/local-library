fcf61f9ec704c6f1d9806ce8ccdd640be1fb248b
DonnieAndMooie <owenjeaves@gmail.com>
2023-05-03 23:18:26 +0100

    Create genre list page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -2,7 +2,12 @@ const asyncHandler = require("express-async-handler");
 const Genre = require("../models/genre");
 
 exports.genre_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre list");
+  const allGenres = await Genre.find({}).sort({ name: 1 }).exec();
+
+  res.render("genre_list", {
+    title: "Genre List",
+    genre_list: allGenres,
+  });
 });
 
 exports.genre_detail = asyncHandler(async (req, res, next) => {
